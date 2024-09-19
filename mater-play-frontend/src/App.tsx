737fd9b81a637e0099984d4222233cd9cc6edfa8
Vinicius Lourenzoni <viniciuslourenzoni0@gmail.com>
2024-09-19 20:09:16 -0300

    ajustes

--- mater-play-frontend/src/App.tsx
+++ mater-play-frontend/src/App.tsx
@@ -4,39 +4,7 @@ import MovieCard from "./app/components/MovieCard";
 import Section from "./app/components/Section";
 import HighlightSection from "./app/components/HighlightSection";
 import { Container } from "@mui/material";
-
-export const movies = [
-  {
-    poster: "/assets/poster1.jpg",
-  },
-  {
-    poster: "/assets/poster2.jpg",
-  },
-  {
-    poster: "/assets/poster3.jpg",
-  },
-  {
-    poster: "/assets/poster4.jpg",
-  },
-  {
-    poster: "/assets/poster5.jpg",
-  },
-  {
-    poster: "/assets/poster6.jpg",
-  },
-  {
-    poster: "/assets/poster7.jpg",
-  },
-  {
-    poster: "/assets/poster8.jpg",
-  },
-  {
-    poster: "/assets/poster9.jpg",
-  },
-  {
-    poster: "/assets/poster10.jpg",
-  },
-];
+import { movies } from "./app/consts/const";
 
 function App() {
   return (
---
+++ mater-play-frontend/src/app/consts/const.ts
@@ -0,0 +1,32 @@
+export const movies = [
+  {
+    poster: "/assets/poster1.jpg",
+  },
+  {
+    poster: "/assets/poster2.jpg",
+  },
+  {
+    poster: "/assets/poster3.jpg",
+  },
+  {
+    poster: "/assets/poster4.jpg",
+  },
+  {
+    poster: "/assets/poster5.jpg",
+  },
+  {
+    poster: "/assets/poster6.jpg",
+  },
+  {
+    poster: "/assets/poster7.jpg",
+  },
+  {
+    poster: "/assets/poster8.jpg",
+  },
+  {
+    poster: "/assets/poster9.jpg",
+  },
+  {
+    poster: "/assets/poster10.jpg",
+  },
+];
